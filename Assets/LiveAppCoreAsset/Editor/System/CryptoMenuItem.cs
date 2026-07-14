db1b4f312668a23075b8c7283bc6399e1e93f0bf
CHOI.JY <choi.jaeyeon@weavr-corp.com>
2026-07-14 17:04:49 +0900

    Commit 실수

--- Assets/LiveAppCoreAsset/Editor/System/CryptoMenuItem.cs
+++ Assets/LiveAppCoreAsset/Editor/System/CryptoMenuItem.cs
@@ -1,17 +1,18 @@
 using Cysharp.Threading.Tasks;
 using LiveAppCore.Editor.View;
 using UnityEditor;
+using UnityEngine;
 
 namespace LiveAppCore.Editor
 {
     /// <summary>
-    /// Rom BUild 관련 Unity Custom Menu
+    /// 암호화/복호화 관련 Unity Custom Menu
     /// </summary>
-    public class RomBuildMenuItem
+    public class CryptoMenuItem
     {
-        private const string MENU_NAME_iOS_BUILD_ONLY = "LiveAppTool/ROM(iOS) Build";
-        [MenuItem( MENU_NAME_iOS_BUILD_ONLY, priority = 10 )]
-        private static async UniTask<bool> iosRomBuild()
+        private const string MENU_NAME_ENCRYPTION = "LiveAppTool/File Crypto/Encryption";
+        [MenuItem( MENU_NAME_ENCRYPTION, priority = 20 )]
+        private static async UniTask<bool> FileEncrypt()
         {
             var platform = EditorUserBuildSettings.activeBuildTarget.ToString();
             return await RomBuildView.ExecuteRomBuild( platform );
