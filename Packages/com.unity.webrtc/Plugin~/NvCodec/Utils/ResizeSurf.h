a08e600b694405e9a3493434f213c61ebf678a53
CHOI.JY <choi.jaeyeon@weavr-corp.com>
2026-08-21 13:25:56 +0900

    commit

--- Packages/com.unity.webrtc/Plugin~/NvCodec/Utils/ResizeSurf.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <cuda.h>
-
-CUresult ResizeSurf(CUarray srcArray, CUarray dstArray);
--- Packages/com.unity.webrtc/Plugin~/WebRTCPlugin/Android/Jni.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace unity
-{
-namespace webrtc
-{
-    bool IsVMInitialized();
-}
-}
